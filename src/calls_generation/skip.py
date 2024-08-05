7c7b29301da29a33879e99e9341aaf9f246f9e5a
amit-elbaz <amite@iguazio.com>
2024-08-05 12:10:31 +0000

    Artifact name is different so I changed it in the save_current_example_data() function

--- src/calls_generation/skip.py
+++ src/calls_generation/skip.py
@@ -106,7 +106,7 @@ def save_current_example_data():
         ("conversation-generation_metadata", "conversation_generation/metadata.zip"),
         ("conversation-generation_ground_truths", "conversation_generation/ground_truths.zip"),
         ("text-to-audio_audio_files", "text_to_audio/audio_files.zip"),
-        ("text-to-audio_audio_files_dataframe", "text_to_audio/dataframe.zip"),
+        ("text-to-audio_dataframe", "text_to_audio/dataframe.zip"),
         ("batch-creation_calls_batch", "batch_creation/calls_batch.zip"),
     ]:
         export_path = export_dir / target_path
